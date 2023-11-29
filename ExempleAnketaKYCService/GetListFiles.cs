34251634eea704911e75181dcae148fd4aaf9ee1
Alexxey <AlexKv2000@mail.ru>
2023-11-29 23:29:23 +0300

    add GetListFiles.cs

---
+++ ExempleAnketaKYCService/GetListFiles.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using GAZ.AnketaKYC;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ExempleAnketaKYCService
+{
+    public class GetListFiles
+    {
+        public static async Task Main(string[] args)
+        {
+        //     Task<List<string>> list = null;
+        //     // await Task.Run(() =>
+        //     // {
+        //         list = GetList(args[0]);
+        //     // });
+        //     
+        // }
+        //
+        // private static async Task<List<String>> GetList(string s)
+        // {
+            // Создаем rez - XML строку
+            string rez = null;
+            string URL =
+                $"https://kyc-compliance.ru/api/download_questionnaires.php?ID={args[0]}"; //Получение списка id анкет готовых для выгрузки
+            //Console.WriteLine($"Ссылка на анкету : {URL}");
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(URL);
+                client.DefaultRequestHeaders.Authorization = Program.AuthorizationKYC.AuthorKYC;
+                rez = await client.GetStringAsync(URL).ConfigureAwait(false);
+            }
+
+            //Console.WriteLine(rez);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(rez);
+
+            // Получаем все элементы с атрибутом xlink
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("//document[@xlink]");
+            // Получаем значения выбранных атрибутов и выводим их
+            List<string> Attribute = null;
+            foreach (XmlNode node in nodes)
+            {
+                string selectedAttribute = node.Attributes["xlink"].Value;
+                Console.WriteLine(selectedAttribute);
+                // Attribute.Add(selectedAttribute);
+            }
+            // return Attribute;
+        }
+    }
+}
+
+
+public class AuthorizationKYC
+{
+    public static string username = "api";
+    public static string pwd = "szuWm7^8S184S05%FNy!";
+
+    public static readonly AuthenticationHeaderValue AuthorKYC = new AuthenticationHeaderValue("Basic",
+        Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes($"{username}:{pwd}")));
+
+    public AuthenticationHeaderValue AuthorKyc { get; }
+}
